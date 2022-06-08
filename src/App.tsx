e476b31f75851a317346c3e8d4e008bb4a65d8f3
Mario B. Santana <purrgat6ry@gmail.com>
2022-06-08 15:31:34 -0500

    S2

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import Appbar from "./microcomponent/Appbar";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTheme } from "@mui/material/styles";
+import {Dates} from "./microcomponent/displayDate/displayDate";
 
-export default function App() {
+export interface AppProps {
+  dates ?: Dates ;
+}
+
+export default function App({dates}: AppProps) {
   const theme = useTheme();
   const matches = useMediaQuery(theme.breakpoints.down("md"));
+  console.log("fechas desde el contenedor", dates);
   return (
     <div>
       <Appbar matchesSM={matches} />
