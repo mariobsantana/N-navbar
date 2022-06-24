da3a307099d26142e03cd9f52b295ff15e756ca2
Karla Nasabi Santiago <nasabisan@gmail.com>
2022-06-24 09:34:36 -0500

    Codigo reparado

--- src/microcomponent/Drawer/Drawer.stories.tsx
+++ src/microcomponent/Drawer/Drawer.stories.tsx
@@ -1,20 +1,20 @@
-import * as React from "react";
-import { Story, Meta } from "@storybook/react";
-import MainDrawer from "./Drawer";
+// import * as React from "react";
+// import { Story, Meta } from "@storybook/react";
+// import MainDrawer from "./Drawer";
 
 
-export default {
-    title: "Header/MainDrawer",
-    component: MainDrawer,
-} as Meta;
+// export default {
+//     title: "Header/MainDrawer",
+//     component: MainDrawer,
+// } as Meta;
 
-const Template: Story = (args) => <MainDrawer {...args} />;
+// const Template: Story = (args) => <MainDrawer {...args} />;
 
-export const DefaultDrawer = Template.bind({});
-DefaultDrawer.args = {};
-DefaultDrawer.parameters = {
-    backgrounds: {
-        default: "blue",
-        values: [{ name: "blue", value: "#1976d2" }],
-    },
-};
+// export const DefaultDrawer = Template.bind({});
+// DefaultDrawer.args = {};
+// DefaultDrawer.parameters = {
+//     backgrounds: {
+//         default: "blue",
+//         values: [{ name: "blue", value: "#1976d2" }],
+//     },
+// };
